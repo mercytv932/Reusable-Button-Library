ab422974b648046a5eb25eed47517584110f293f
mercytv932 <Mercytv932@gmail.com>
2026-03-28 19:05:14 -0400

    Add variant prop to Button component

--- Button.js
+++ Button.js
@@ -1,5 +1,5 @@
-function Button({text, onClick}) {
-  return <button onClick ={onClick}>{text}</button>
+function Button({text, onClick, variant}) {
+  return <button className={variant} onClick ={onClick}>{text}</button>
 }
 
 export default Button;
