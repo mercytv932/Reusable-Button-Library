b48e8d8bb703871f056c7ef152ac4938a1622eee
mercytv932 <Mercytv932@gmail.com>
2026-03-27 22:30:25 -0400

    Add files via upload
    feat: add reusable Button component with text and onClick props

---
+++ App.js
@@ -0,0 +1,12 @@
+import Button from './Button';
+
+function App() {
+  return (
+    <div>
+      <Button text="Click me" onClick={() => alert('clicked!')}/>
+    </div>
+  );
+}
+
+export default App;
+
