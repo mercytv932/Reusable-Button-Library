112af91d114146709ab69ab214e4feb7c1f4ff96
mercytv932 <Mercytv932@gmail.com>
2026-03-28 19:38:03 -0400

    Add size prop to Button component

--- Button.js
+++ Button.js
@@ -1,5 +1,5 @@
-function Button({text, onClick, variant}) {
-  return <button className={variant} onClick ={onClick}>{text}</button>
+function Button({text, onClick, variant, size}) {
+  return <button className={`${variant} ${size} `} onClick={onClick}>{text}</button>
 }
 
 export default Button;
