525b127fde3101f4ee673761991e3fa0c56ce1c7
mercytv932 <Mercytv932@gmail.com>
2026-03-30 23:57:41 -0400

    Add PropTypes to Button component
    Added PropTypes for Button component props validation.

--- Button.js
+++ Button.js
@@ -1,4 +1,6 @@
  import styles from './Button.module.css';
+ import PropTypes from 'prop-types';
+
 function Button({text, onClick, variant, size, disabled, isLoading, icon}) {
   return <button 
    className={`${styles [variant]} ${styles [size]} `} 
@@ -11,6 +13,16 @@ function Button({text, onClick, variant, size, disabled, isLoading, icon}) {
     {icon && <span className="button-icon">{icon}</span>}
     { isLoading ?'Loading...' : text}
    </button>
+}
+
+Button.propTypes = {
+  text: PropTypes.string,
+  onClick: PropTypes.func,
+  variant: PropTypes.oneOf(['primary', 'secondary', 'danger']),
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  disabled: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  icon: PropTypes.string
 }
 
 export default Button;
